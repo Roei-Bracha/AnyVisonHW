391a3b7eb610a2aa787c63f13fd3265e390c953f
Roei-Bracha <roei635@gmail.com>
2019-05-17 16:09:13 +0300

    register - work

--- client/src/Components/HomePage/Login/Login.jsx
+++ client/src/Components/HomePage/Login/Login.jsx
@@ -3,6 +3,7 @@ import './Login.scss'
 import './InputRow'
 import {Card ,Button} from "antd"
 import InputRow from "./InputRow";
+import {baseUrl} from '../../../config.js'
 export default function Login (){
     const [mode,setMode] = useState("Login")
     const [username,setUsername] = useState("")
@@ -25,8 +26,39 @@ export default function Login (){
             <span className={"accountChangeMode"} onClick={()=>{setMode(mode==='Login' ? "Register" : "Login")}}>
             {mode==='Login' ? 'Dont have an account?' : "Already have an Account?"}
             </span>
-            <Button>{mode}</Button>
+            <Button onClick={()=>{handleSubmit(mode,username,password,rePassword,email)}}>{mode}</Button>
             </div>
         </Card>
     )
+}
+
+const handleSubmit = (mode,username,password,rePassword,email)=>{
+    if(mode === "Register"){
+        if(password === rePassword)
+        fetch(`${baseUrl}/api/newUser`,{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                username:username.toLowerCase(),
+                password,
+                email
+            })
+        })
+    }
+    else{
+        fetch(`${baseUrl}/api/login`,{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                username,
+                password
+            })
+        }).then(data=>data.json()).then((response)=>{
+            console.log(response)
+        })
+    }
 }
--- server/server.js
+++ server/server.js
@@ -6,21 +6,35 @@ const User = require("./db/Schemas/User");
 const db = require('./db/db');
 const app = express ();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+if(process.env.NODE_ENV === "develop"){
+    const cors = require("cors")
+    app.use(cors())
+}
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.post('/api/newUser',(req,res)=>{
-    bcrypt.hash(req.body.password,5,).then(()=>{
+    bcrypt.hash(req.body.password,5).then((hash)=>{
         const newUser = new User({
             username:req.body.username,
             passwordHash:hash,
             email:req.body.email
         })
-        newItem.save().then(res.sendStatus(200)).catch((err)=>{
-            console.log(err)
+        newUser.save().then(res.sendStatus(200)).catch((err)=>{
+            console.log("eror in saving in mongo:", err)
             res.sendStatus(500)
         });
+    }).catch((err)=>{
+        console.log('bcrypt error:', err)
     });
 })
+
+// app.post("/api/login",(req,res)=>{
+
+// })
 
 
 module.exports = app
